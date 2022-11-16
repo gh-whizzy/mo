c2cd2f3f17f3b1abbf2d5b619883943106d3e226
gh-whizzy <gh.whizzy@gmail.com>
2022-11-16 10:02:05 +0000

    Playlist gets random media type, if audio display array of random ids plucked from the media table with an audio media_type

--- database/factories/PlaylistFactory.php
+++ database/factories/PlaylistFactory.php
@@ -2,7 +2,9 @@
 
 namespace Database\Factories;
 
+use App\Models\Media;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Log;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Playlist>
@@ -16,9 +18,29 @@ class PlaylistFactory extends Factory
      */
     public function definition()
     {
+        $allIdsOfAudio = Media::all()->where('media_type', 'audio')->random(10)->pluck('id');
+        $allIdsOfVideo = Media::all()->where('media_type', 'video')->pluck('id');
+        $allIdsOfImage = Media::all()->where('media_type', 'images')->pluck('id');
+
+        $randomMediaType = Media::all()->random(1)->pluck('media_type');
+        Log::info($randomMediaType[0]);
+        
+
+
+        // Log::info(Media::all()->random(1)->pluck('media_type'));
+        // Log::info(gettype($randomMediaType));
+
+        // if (gettype($randomMediaType) == 'audio') {
+        //     Log::info('yes');
+        // } else {
+        //     Log::info('no');
+        // }
+
         return [
             'playlist_name' => fake()->name(),
-            'playlist_type' => fake()->randomElement(['video', 'audio', 'images']),
+            // 'playlist_type' => fake()->randomElement(['video', 'audio', 'image']),
+            // 'playlist_type' => Media::all()->random()->pluck('media_type') == 'audio' ? $allIdsOfAudio : 'bar',
+            'playlist_type' => $randomMediaType[0] === 'audio' ? $allIdsOfAudio : 'nope',
             'created_by' => fake()->name(),
             'media' => fake()->url()
         ];
