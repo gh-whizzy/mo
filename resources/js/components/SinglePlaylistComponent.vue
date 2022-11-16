9b59aea6c06afa429b80781ce5fc61a7b72d2edc
gh-whizzy <gh.whizzy@gmail.com>
2022-11-16 13:25:01 +0000

    Can now display the data of a playlist in the single playlist component

--- resources/js/components/SinglePlaylistComponent.vue
+++ resources/js/components/SinglePlaylistComponent.vue
@@ -1,5 +1,27 @@
 <template>
-    <div>
+    <div @click="showData()">
         single playlist
+        <div>{{ test.playlist_name }}</div>
+        <div>{{ test.playlist_type }}</div>
+        <div>{{ test.media }}</div>
+        <div>{{ test.created_by}}</div>
+
     </div>
 </template>
+
+<script>
+
+export default {
+    data() {
+        return {}
+    },
+
+    methods: {
+        showData() {
+            // console.log(this.$props.test.selectedPlaylist)
+        }
+    },
+
+    props: ['test']
+}
+</script>
